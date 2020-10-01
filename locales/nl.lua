6c0d5a4e98b04a45d5668d30b796b10f317a45c5
Sander Jochems <sanderjochems@hotmail.nl>
2020-10-01 20:15:57 +0200

    Add Dutch translation

---
+++ locales/nl.lua
@@ -0,0 +1,8 @@
+Locales['nl'] = {
+  ['left_instance'] = 'je bent de instantie verlaten',
+  ['invite_expired'] = 'uitnodiging verlopen',
+  ['press_to_enter'] = 'druk op ~INPUT_CONTEXT~ om de instantie in te voeren',
+  ['entered_instance'] = 'je bent de instantie binnengekomen',
+  ['entered_into'] = '%s is de instantie binnengekomen',
+  ['left_out'] = '%s heeft de instantie verlaten',
+}
